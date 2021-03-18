8108153bd5ce3ec2112ed4542005988022f7aefb
Assem Ali <asimali1996@gmail.com>
2021-03-18 13:11:12 +0200

    adding display_data() function

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -177,6 +177,13 @@ def user_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
+def display_data(df):
+    index = 0
+    user_input = input("Would you like to display 5 rows of raw data? Enter yes or no.\n ").lower()
+    while user_input in ['yes', 'y', 'yep', 'yup', 'yeah'] and index + 5 < df.shape[0]:
+        print(df.iloc[index : index + 5])
+        index += 5
+        user_input = input("Would you like to display more 5 rows of raw data? Enter yes or no.\n ").lower()
 
 def main():
     while True:
@@ -187,6 +194,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
+        display_data(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
